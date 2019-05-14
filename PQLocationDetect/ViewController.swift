a5316679c5de8d588e27012b744a0553b4f6a874
Reng Tsai <Rengaht@users.noreply.github.com>
2019-05-14 11:35:52 +0800

    Initial Commit

---
+++ PQLocationDetect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PQLocationDetect
+//
+//  Created by RengTsai on 2019/5/14.
+//  Copyright © 2019 RengTsai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

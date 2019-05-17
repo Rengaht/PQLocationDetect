8716c1cbf1904ba597ab8c81c76191427283d46d
Reng Tsai <Rengaht@users.noreply.github.com>
2019-05-18 02:05:23 +0800

    backup

--- PQLocationDetect/ViewController.swift
+++ PQLocationDetect/ViewController.swift
@@ -7,14 +7,59 @@
 //
 
 import UIKit
+import CoreMotion
+import CoreLocation
+import SwiftOSC
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    let motionManager = CMMotionManager()
+    var timer: Timer!
+    var client = OSCClient(address: "192.168.0.100", port: 8080)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        motionManager.startAccelerometerUpdates()
+        motionManager.startGyroUpdates()
+        motionManager.startMagnetometerUpdates()
+        motionManager.startDeviceMotionUpdates()
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0/30.0, target: self, selector: #selector(ViewController.update), userInfo: nil, repeats: true)
     }
-
-
+    
+    @objc func update() {
+//        if let accelerometerData = motionManager.accelerometerData {
+//            print(accelerometerData.acceleration)
+//            let message = OSCMessage(
+//                OSCAddressPattern("/pos"),
+//                accelerometerData.acceleration.x,
+//                accelerometerData.acceleration.y,
+//                accelerometerData.acceleration.z
+//            )
+//            client.send(message)
+//        }
+        let gyroData = motionManager.gyroData;
+        let magnetometerData = motionManager.magnetometerData;
+        
+        let deviceMotion = motionManager.deviceMotion;
+        
+//        print(deviceMotion);
+        
+        let message = OSCMessage(
+            OSCAddressPattern("/acc"),
+            deviceMotion?.userAcceleration.x,
+            deviceMotion?.userAcceleration.y,
+            deviceMotion?.userAcceleration.z,
+            deviceMotion?.attitude.pitch,
+            deviceMotion?.attitude.roll,
+            deviceMotion?.attitude.yaw
+        );
+        //print(message);
+        client.send(message)
+            
+        
+      
+    }
 }
 
